da693b0404b7152dd94ab5a6ffb2565e733f03dc
Przemyslaw Thomann <peerzet3@gmail.com>
2020-08-17 00:04:15 +0200

    Fix

--- src/pages/password-reset/ResetPasswordPage.vue
+++ src/pages/password-reset/ResetPasswordPage.vue
@@ -1,24 +1,22 @@
 <template>
   <q-page>
-    <div :class="{ 'fixed-center': !$q.platform.is.mobile }">
-      <div class="row justify-center text-center items-center">
-        <div class="col-auto">
-          <q-card class="shadow-10 bg-grey-2" :style="$q.platform.is.mobile ? `width: ${screenWidth * 0.9}px` : `width: ${screenWidth * 0.2}px`">
-            <q-card-section :class="{ 'q-pa-lg': !$q.platform.is.mobile }">
-              <bittery-logo-animated></bittery-logo-animated>
-              <div class="text-primary q-pt-md text-h5"
-                   :style="$q.platform.is.mobile ? `width: ${screenWidth * 0.8}px` : `width: ${screenWidth * 0.18}px`">
-                Reset your password
-              </div>
-              <div class="text-primary text-subtitle1">
-                Please provide your registration e-mail address
-              </div>
-            </q-card-section>
-            <q-card-section style="padding-top: 0; margin-top:0">
-              <reset-password-form></reset-password-form>
-            </q-card-section>
-          </q-card>
-        </div>
+    <div class="row justify-center text-center items-center" :style="'height:' + innerScreenHeight + 'px !important;'">
+      <div class="col-auto">
+        <q-card class="shadow-10 bg-grey-2" :style="$q.platform.is.mobile ? `width: ${screenWidth * 0.9}px` : `width: ${screenWidth * 0.2}px`">
+          <q-card-section :class="{ 'q-pa-lg': !$q.platform.is.mobile }">
+            <bittery-logo-animated></bittery-logo-animated>
+            <div class="text-primary q-pt-md text-h5"
+                 :style="$q.platform.is.mobile ? `width: ${screenWidth * 0.8}px` : `width: ${screenWidth * 0.18}px`">
+              Reset your password
+            </div>
+            <div class="text-primary text-subtitle1">
+              Please provide your registration e-mail address
+            </div>
+          </q-card-section>
+          <q-card-section style="padding-top: 0; margin-top:0">
+            <reset-password-form></reset-password-form>
+          </q-card-section>
+        </q-card>
       </div>
     </div>
   </q-page>
