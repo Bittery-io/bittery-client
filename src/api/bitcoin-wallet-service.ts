4443fac7ecd94d643f6abf46456dac13f057bfea
Przemyslaw Thomann <peerzet3@gmail.com>
2020-09-21 22:02:50 +0200

    Fix for public key from seed

--- src/api/bitcoin-wallet-service.ts
+++ src/api/bitcoin-wallet-service.ts
@@ -1,14 +1,17 @@
 import { generateMnemonic } from 'bip39';
 // @ts-ignore
-import * as Bip84 from 'bip84/src/index';
+import * as bip84 from 'bip84';
 
 import { BitcoinWallet } from 'src/model/bitcoin-wallet';
 
 export const generateBitcoinWallet = (): BitcoinWallet => {
   const seed: string = generateMnemonic();
-  const root = new Bip84.fromSeed(seed);
+  const root = new bip84.fromSeed(seed, true);
+  const child0 = root.deriveAccount(0);
+  const account0 = new bip84.fromZPrv(child0);
+  console.log('Public key 0:', account0.getAccountPublicKey(0))
   return new BitcoinWallet(
     seed,
-    root.getRootPublicKey(),
+    account0.getAccountPublicKey(0),
   );
 };
